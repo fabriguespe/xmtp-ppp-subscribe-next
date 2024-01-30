05ad6f7164dd1137374505adc5032f6a796ebabd
fabri <fguespe@gmail.com>
2024-01-30 01:51:10 -0300

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# XMTP Quickstart
+
+## Installation
+
+```bash
+bun install
+bun start
+```
+
+## Concepts
+
+Head to our docs to understand XMTP's concepts
+
+- [Get started](https://xmtp.org/docs/build/get-started/overview?sdk=js)
+- [Authentication](https://xmtp.org/docs/build/authentication?sdk=js)
+- [Conversations](https://xmtp.org/docs/build/conversations?sdk=js)
+- [Messages](https://xmtp.org/docs/build/messages/?sdk=js)
+- [Streams](https://xmtp.org/docs/build/streams/?sdk=js)
+
+#### Troubleshooting
+
+If you get into issues with `Buffer` and `polyfills` check out the fix below:
+
+- [Check out Buffer issue](https://github.com/xmtp/xmtp-js/issues/487)
---
+++ src/app/page.js
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Subscribe } from "./Subscribe";
+
+const InboxPage = () => {
+  const [subscribeArray, setSubscribeArray] = useState([]);
+
+  const styles = {
+    homePageWrapper: {
+      textAlign: "center",
+      marginTop: "2rem",
+    },
+  };
+
+  return (
+    <div style={styles.homePageWrapper}>
+      <h1>Subscribe Button </h1>
+
+      <Subscribe
+        senderAddress="0x93E2fc3e99dFb1238eB9e0eF2580EFC5809C7204"
+        onSubscribe={(address, state) => {
+          console.log("New subscriber: ", { address, state });
+          setSubscribeArray((prevArray) => [...prevArray, { address, state }]);
+        }}
+        onUnsubscribe={(address, state) => {
+          console.log("Unsubscribed: ", { address, state });
+          setSubscribeArray((prevArray) => {
+            const index = prevArray.findIndex((a) => a.address === address);
+            if (index !== -1) {
+              const newArray = [...prevArray];
+              newArray[index].state = state;
+              return newArray;
+            }
+            return prevArray;
+          });
+        }}
+        onError={(error) => console.log("Error subscribing: " + error)}
+        env="production"
+      />
+    </div>
+  );
+};
+
+export default InboxPage;
